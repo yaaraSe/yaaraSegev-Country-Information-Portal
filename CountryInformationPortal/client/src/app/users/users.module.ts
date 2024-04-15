830f766796325535c7ab30ee3ba6c30ed9a9e5f6
Yaara Segev <Yaara.Segev@agamimp.net>
2024-04-15 08:47:52 +0300

    Angular 17- part 18/ modules

---
+++ CountryInformationPortal/client/src/app/users/users.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddUserComponent } from './add-user/add-user.component';
+
+@NgModule({
+  declarations: [AddUserComponent],
+  imports: [CommonModule],
+})
+export class UsersModule {}

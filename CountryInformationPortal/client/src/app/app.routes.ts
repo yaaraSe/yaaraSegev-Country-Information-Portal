6a8c10f10d5853d54da8a6298d790373c127501b
Yaara Segev <Yaara.Segev@agamimp.net>
2024-04-15 10:50:48 +0300

    add routing

--- CountryInformationPortal/client/src/app/app.routes.ts
+++ CountryInformationPortal/client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'add-user',
+    loadComponent: () =>
+      import('./admin/add-user/add-user.component').then(
+        (c) => c.AddUserComponent
+      ),
+  },
+];
